9ceb67dcddbe4e2254b147634c85b1b4b1891b96
spurgit <spurisa17@ku.edu.tr>
2023-02-09 02:10:01 +0300

    initial version

---
+++ inventorycounter.py
@@ -0,0 +1,31 @@
+def read_inventory(filename):
+    infile = open(filename, 'r')
+    package_list = []
+    for line in infile:
+        package_content = line.rstrip()
+        package_list.append(package_content)
+    infile.close()
+    return package_list
+
+def analyze_inventory(inventory):
+    package_count = len(inventory)
+    items_dict = {}
+    for package in inventory:
+        inside = package.split(", ")
+        for element in inside:
+            content = element.split(" ")
+            count = int(content[0])
+            item_as_list = content[1:]
+            item = " ".join(item_as_list)
+            if item in items_dict:
+                items_dict[item] += count
+            else:
+                items_dict[item] = count
+    return  package_count, items_dict
+
+envanter = read_inventory("kolilistesi.txt")
+koli_sayisi, icerik = analyze_inventory(envanter)
+print("Totalde", koli_sayisi, "koli var.")
+print("Bu koliler içerisinde şunlar bulunuyor:")
+for key in icerik.keys():
+    print("-" + str(icerik[key]), key)
